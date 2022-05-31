68bfe7370f9b4905ed565f9704cec0365d76f5b9
Temiogundejim <yusuffogundeji@gmail.com>
2022-05-31 07:44:18 +0100

    changes made

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from flask import Flask, jsonify, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
+
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Temilorun123_@localhost:5432/todoapp'
@@ -13,42 +14,44 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-class Todo(db.Model):
-    __tablename__ = 'todos'
-    id = db.Column(db.Integer, primary_key=True)
-    description = db.Column(db.String(), nullable=False)
-    completed = db.Column(db.Boolean, nullable=False, default=False)
-    def __repr__(self):
-        return f'<Person ID: {self.id}, name: {self.description}'
 
-db.create_all()
+class Todo(db.Model):
+  __tablename__ = 'todos'
+  id = db.Column(db.Integer, primary_key=True)
+  description = db.Column(db.String(), nullable=False)
+  completed = db.Column(db.Boolean, nullable=False, default=False)
+
+  def __repr__(self):
+    return f'<Todo {self.id} {self.description}>'
 
 @app.route('/todos/create', methods=['POST'])
-def create_todo():
-    body = {}
-    error = False
-
-    try:
-        description = request.get_json()['description']
-        todo = Todo(description=description)
-        body['description'] = todo.description
-        db.session.add(todo)
-        db.session.commit()
-    except:
+def create_todo():   
+   body={}
+   error = False
+   try: 
+       description =  request.get_json()['description']
+       todo = Todo(description=description)
+       body['description'] = todo.description
+       db.session.add(todo)
+       db.session.commit()
+   except:        
         error = True
         db.session.rollback()
         print(sys.exc_info())
-    finally:
-        db.session.close()
-        if error == True: 
+   finally:
+        db.session.close()           
+        if  error == True:
             abort(400)
-        else:
+        else:            
             return jsonify(body)
 
-@app.route('/home')
-def index(): 
-    return render_template('./templates/index.html', data=Todo.query.all)
+@app.route('/')
+def index():
+    return render_template('index.html', data=Todo.query.all())
 
+
+
+
+#always include this at the bottom of your code
 if __name__ == '__main__':
-    app.debug = True
-    app.run(host='127.0.0.1', port=3000)
+   app.run(host="0.0.0.0", port=3000)
