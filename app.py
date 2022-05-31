c1dfe8ba8c293bf7ae8a683052d8eb2995a3d50f
Temiogundejim <yusuffogundeji@gmail.com>
2022-05-31 07:21:16 +0100

    setup app

---
+++ app.py
@@ -0,0 +1,54 @@
+from email.policy import default
+from os import abort
+import sys
+from flask import Flask, jsonify, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Temilorun123_@localhost:5432/todoapp'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+class Todo(db.Model):
+    __tablename__ = 'todos'
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(), nullable=False)
+    completed = db.Column(db.Boolean, nullable=False, default=False)
+    def __repr__(self):
+        return f'<Person ID: {self.id}, name: {self.description}'
+
+db.create_all()
+
+@app.route('/todos/create', methods=['POST'])
+def create_todo():
+    body = {}
+    error = False
+
+    try:
+        description = request.get_json()['description']
+        todo = Todo(description=description)
+        body['description'] = todo.description
+        db.session.add(todo)
+        db.session.commit()
+    except:
+        error = True
+        db.session.rollback()
+        print(sys.exc_info())
+    finally:
+        db.session.close()
+        if error == True: 
+            abort(400)
+        else:
+            return jsonify(body)
+
+@app.route('/home')
+def index(): 
+    return render_template('./templates/index.html', data=Todo.query.all)
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='127.0.0.1', port=3000)
